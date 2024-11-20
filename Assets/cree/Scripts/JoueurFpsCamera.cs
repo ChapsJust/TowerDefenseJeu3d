cb1fc1c507ec9c01c0497c18cbd703f301f7ebde
chapsjust <2238424@carrefour.cegepvicto.ca>
2024-11-20 16:23:33 -0500

    Joueur Bouger + Camera + RigidBody + CollisionSol

---
+++ Assets/cree/Scripts/JoueurFpsCamera.cs
@@ -0,0 +1,46 @@
+using System.Net.NetworkInformation;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class JoueurFpsCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform joueurBody;
+    [SerializeField]
+    private Transform fpsCamera;
+
+    [SerializeField]
+    private float sensitivity = 80f;
+    [SerializeField]
+    private Vector2 sourisInput = Vector2.zero;
+
+    private float xRotation = 0f;
+    private float smoothPitchVelocity = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void LateUpdate()
+    {
+        if (sourisInput != Vector2.zero)
+        {
+            float sourisX = sourisInput.x * sensitivity * Time.deltaTime;
+            joueurBody.Rotate(Vector3.up, sourisX);
+
+            float sourisY = sourisInput.y * sensitivity * Time.deltaTime;
+            xRotation -= sourisY;
+            xRotation = Mathf.Clamp(xRotation, -70, 40f);
+
+
+            fpsCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        }
+    }
+
+    public void OnLook(InputAction.CallbackContext contexte)
+    {
+        sourisInput = contexte.ReadValue<Vector2>();
+    }
+}
--- Assets/cree/Scripts/JoueurFpsControlleur.cs
+++ Assets/cree/Scripts/JoueurFpsControlleur.cs
@@ -1,16 +1,51 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class JoueurFpsControlleur : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private Transform joueurBody;
+
+    private Rigidbody rb;
+
+    private Vector2 mouvementInput;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        MoveJoueur();
+    }
+
+    private void MoveJoueur()
+    {
+        Vector3 forward = joueurBody.forward;
+        Vector3 right = joueurBody.right;
+
+        forward.y = 0f;
+        right.y = 0f;
+
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 direction = forward * mouvementInput.y + right * mouvementInput.x;
+
+        Vector3 mouvement = direction * 5f;
+        rb.linearVelocity = new Vector3(mouvement.x, rb.linearVelocity.y, mouvement.z);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        Debug.Log("1");
+        mouvementInput = context.action.ReadValue<Vector2>();
     }
 }
+
